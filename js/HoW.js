0ba20d4c4821f024d4709e15d4b74f8f5eec9fd2
Francesca Manto <fmanto@ucla.edu>
2015-02-05 23:36:49 -0800

    added lorraine schneider vid and quote

--- js/HoW.js
+++ js/HoW.js
@@ -11,23 +11,30 @@
 
     var people_data = [
     {
+        name: "Lorraine Schneider",
+        quote: "My advice to my younger self would be to not think as much as I do or as I did... to not overthink everything, to be more spontaneous, more open to new ideas...",
+        id: 1,
+        video: "https://www.youtube.com/embed/kj7d-ZWgDxc",
+        img: "img/lorraine-schneider.jpg"
+    },
+    {
         name: "Jean Kevin Aguirre",
         quote: "Well I started dressing like this a couple of months after my great-grandfather passed away.",
-        id: 1,
+        id: 2,
         video: "https://www.youtube.com/embed/twmdGglKV9w",
         img: "img/jean-aguirres.jpg"
     },
     {
         name: "Christian Green",
         quote: "Let go of the fear. Let go of the mistrust and be able to allow people to sow good seeds and sow good thoughts.",
-        id: 2,
+        id: 3,
         video: "https://www.youtube.com/embed/B_15_oU7yvs",
         img: "img/christian-green.jpg"
     }, 
     {
         name: "Blanca Cortes",
         quote: "I am a volunteer for the Make-A-Wish Foundation. My official title is a 'wish granter.'",
-        id: 3,
+        id: 4,
         video: "https://www.youtube.com/embed/T8QctHh6UPk",
         img: "img/blanca-cortes.jpg"
     }]; 
